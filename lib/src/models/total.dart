990487de7fd501f57d88b69d3ed94d240aa8f6e5
Marcus Brasizza <mvbdesenvolvimento@gmail.com>
2022-03-15 09:42:35 -0300

    fiscal

--- lib/src/models/total.dart
+++ lib/src/models/total.dart
@@ -19,12 +19,9 @@ class Total {
   factory Total.fromMap(Map<String, dynamic> map) {
     return Total(
       valorTotal: (map['ICMSTot']['vProd']),
-      desconto: (map.containsKey('vCFe')
-              ? (map['DescAcrEntr']?['vDescSubtot'])
-              : map['ICMSTot']['vDesc']) ??
-          '0.00',
+      desconto: (map.containsKey('vCFe') ? (map['DescAcrEntr']?['vDescSubtot']) : map['ICMSTot']['vDesc']) ?? '0.00',
       acrescimo: (map.containsKey('vCFe') ? '0.00' : map['ICMSTot']['vOutro']),
-      valorPago: (map['ICMSTot']['vNF']),
+      valorPago: ((map.containsKey('vCFe') ? map['vCFe'] : map['ICMSTot']['vNF'])) ?? '0.00',
     );
   }
 
